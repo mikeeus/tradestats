282e0ee2d4db61a408b008778e4cdfa5304bb44b
mikeeus <itsmikias@gmail.com>
2017-09-23 22:02:55 +0300

    Create home reducer,actions,effects and selectors

--- src/app/store/home/home.effects.ts
+++ src/app/store/home/home.effects.ts
@@ -12,49 +12,19 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class HomeEffects {
   /**Handle http request errors*/
-  @Effect({ dispatch: false })
-  httpErrors: Observable<any> = this.actions
-    .ofType(
-      HomeActions.ActionTypes.HTTP_ERROR,
-    ).map((action: HomeActions.HttpError) => action.payload)
-    .map(error => {
-      const parsed = this.parseError(error);
-      this.notify.error(parsed.title, parsed.content);
-    });
+  // @Effect({ dispatch: false })
+  // httpErrors: Observable<any> = this.actions
+  //   .ofType(
+  //     HomeActions.ActionTypes.LOAD_GRAPH,
+  //   ).map((action: HomeActions.LoadGraph) => action.payload)
+  //   .map(error => {
+  //     const parsed = this.parseError(error);
+  //     this.notify.error(parsed.title, parsed.content);
+  //   });
 
   constructor(
     private actions: Actions,
     private store: Store<State>,
     private notify: NotificationsService
   ) { }
-
-  /**Parse incoming Error object to render notification title and content.*/
-  parseError(error: Error): { title: string, content: string } {
-    console.log('error:', error);
-    const title = error.name || 'Oops';
-    if (error['status']) {
-      const status = error['status'].toString();
-      switch(status) {
-        case '504':
-        return { title: 'Timeout', content: 'There may be a problem with your internet connection.'}
-        case status.slice(0,1) === '5':
-          return { title: title, content: 'Something went wrong.' };
-      }
-    }
-
-    if (error.message) {
-      return { title: title, content: error.message };
-    }
-    if (error['_body'] && typeof error['_body'] === 'string') {
-      const body = JSON.parse(error['_body']);
-      if (body.message) {
-        return { title: title, content: body.message };
-      }
-      if (body.error) {
-        return { title: title, content: body.error };
-      }
-    }
-    return { title: title, content: 'Something went wrong.' };
-  }
-
 }
