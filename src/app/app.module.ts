b35a35ce5980c011b5317e68e182024acc8d1710
mikeeus <itsmikias@gmail.com>
2017-09-23 20:45:00 +0300

    Configure effects, store, aterial, and other module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { MdProgressBarModule, MdIconModule, MD_PLACEHOLDER_GLOBAL_OPTIONS } from '@angular/material';
 
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
+// Vendor
+import { PerfectScrollbarModule, PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { SimpleNotificationsModule } from 'angular2-notifications';
+
+// State
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffectsModule } from './store/app-effects.module';
+import { reducers } from './store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { AppComponent } from './app.component';
 // App Modules
 import { LayoutModule } from './common';
 
 // Other
 import { TimeoutInterceptor } from './_interceptors';
 
+// Vendor Configuration
+const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true,
+  wheelSpeed: 1
+};
+
+// App Providers
 const APP_PROVIDERS = [
-  { provide: HTTP_INTERCEPTORS, useClass: TimeoutInterceptor, multi: true }
+  { provide: HTTP_INTERCEPTORS, useClass: TimeoutInterceptor, multi: true },
+  {provide: MD_PLACEHOLDER_GLOBAL_OPTIONS, useValue: { float: 'never' }}
 ]
 
 @NgModule({
@@ -26,6 +47,18 @@ const APP_PROVIDERS = [
     FormsModule,
     HttpModule,
     HttpClientModule,
+    MdProgressBarModule,
+    MdIconModule,
+
+    // Vendor
+    PerfectScrollbarModule.forRoot(PERFECT_SCROLLBAR_CONFIG),
+    SimpleNotificationsModule.forRoot(),
+
+    // Store
+    StoreModule.forRoot(reducers),
+    AppEffectsModule,
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 20 }) : [],
 
     LayoutModule
   ],
