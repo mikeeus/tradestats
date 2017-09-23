250e97d4c0ec8685d211f88b6bc99102ca73410d
mikeeus <itsmikias@gmail.com>
2017-09-23 22:44:49 +0300

    Import ChartsModule and include chart.js

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { MdListModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 import { HomeComponent } from './home.component';
 import { ROUTES } from './home.routes';
@@ -15,6 +16,7 @@ import { ROUTES } from './home.routes';
     RouterModule.forChild(ROUTES),
 
     NgxChartsModule,
+    ChartsModule,
 
     MdListModule,
     FlexLayoutModule,
